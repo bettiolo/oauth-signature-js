2427705270887409a4433e28a842a7b2a214babd
Marco Bettiolo <marco@bettiolo.it>
2013-08-08 00:58:11 +0100

    Writing test and implementation of base string generator

--- js/oauth-signature.js
+++ js/oauth-signature.js
@@ -1,14 +1,51 @@
-// Signature Base http://oauth.net/core/1.0/#sig_base_example
-
+// Specification: http://oauth.net/core/1.0/#anchor14
 var SignatureBaseString = (function () {
-    "use strict";
-    function SignatureBaseString() {
+    'use strict';
 
+    function SignatureBaseString(httpMethod, requestUrl, parameters) {
+        this._httpMethod = httpMethod || '';
+        this._requestUrl = requestUrl || '';
+        this._parameters = parameters;
+    };
+
+    SignatureBaseString.prototype._normalizeHttpMethod = function () {
+        // make uppercase
+        this._httpMethod = this._httpMethod.toUpperCase();
+    };
+
+    SignatureBaseString.prototype._normalizeRequestUrl = function () {
+        // HTTP://Example.com:80/resource?id=123
+        // http://example.com/resource
+
+    };
+
+    // lexicographical byte value ordering by name and value
+    SignatureBaseString.prototype._sortParameters = function () {
+
+    };
+
+    SignatureBaseString.prototype._normalizeParameters = function () {
+
+    };
+
+    SignatureBaseString.prototype._concatenateParameters = function() {
+        return '';
+    };
+
+    SignatureBaseString.prototype._concatenateRequestElements = function () {
+        // HTTP_METHOD + request url + parameters
+        var concatenatedParameters = this._concatenateParameters();
+        return this._httpMethod + '&' + this._requestUrl + '&' + concatenatedParameters;
     };
 
     SignatureBaseString.prototype.generate = function() {
-        return 'OK';
+        this._normalizeHttpMethod();
+        this._normalizeRequestUrl();
+        this._sortParameters();
+        this._normalizeParameters();
+        return this._concatenateRequestElements();
     };
 
     return SignatureBaseString;
+
 })();
--- test/oauth-signature.qunit.tests.js
+++ test/oauth-signature.qunit.tests.js
@@ -1,8 +1,23 @@
-module('Oauth Signature');
-test('Signature base string generator', function() {
-    var signatureBaseString = new SignatureBaseString();
-
-
-    equal(signatureBaseString.generate(), 'OK', 'The get method returns OK');
-
+module('Oauth Signature Base String');
+test('It should start with an uppercase http method, followed by two ampersands', function () {
+	equal(new SignatureBaseString('get').generate(), 'GET&&',
+		'Should be GET&& because & should be included for omitted elements');
+	equal(new SignatureBaseString('pOsT').generate(), 'POST&&',
+		'Should be POST&& because & should be included for omitted elements');
+	equal(new SignatureBaseString('').generate(), '&&',
+		'Should be && because the http method is empty');
+	equal(new SignatureBaseString().generate(), '&&',
+		'Should be && because the http method is not passed');
+	equal(new SignatureBaseString(null).generate(), '&&',
+		'Should be && because the http method is null');
+	equal(new SignatureBaseString(undefined).generate(), '&&',
+		'Should be && because the http method is undefined');
+});
+test('The resource url should be included in the second segment after the http method and should be suffixed by an ampersand', function () {
+	equal(new SignatureBaseString('GET', 'http://example.org').generate(), 'GET&http://example.org&',
+		'Should be GET&http://example.org&');
+	equal(new SignatureBaseString('', 'hTTp://EXAMPLE.ORG/endpoint').generate(), '&http://example.org/endpoint',
+		'Should be &http://example.org/endpoint because the URL scheme and authority must be lowercase');
+	equal(new SignatureBaseString('', 'http://example.org:80/endpoint').generate(), '&http://example.org/endpoint',
+		'Should be &http://example.org/endpoint because the default port http (80) and https (443) must be excluded');
 });
