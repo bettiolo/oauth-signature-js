b801e43b50fd26a20d9976cff1976e81c97dfde5
Marco Bettiolo <marco.bettiolo@7digital.com>
2013-08-08 09:53:11 +0100

    Clean-up of jasmine tests, runners and support files. Clean-up of modernizr

--- test/oauth-signature.jsamine.tests.js
+++
@@ -1,7 +0,0 @@
-describe("Signature base string generator tests", function() {
-    var signatureBaseString = new SignatureBaseString();
-
-    it("should return OK when getting the base string", function() {
-        expect(signatureBaseString.generate()).toBe('OK');
-    });
-});
